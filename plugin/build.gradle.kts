c4ea9249ef4996f55eec5b4971c543af902d8d8c
Luis Mejia <lmejia@gmail.com>
2020-11-28 04:09:37 -0800

    Added tests.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,20 @@
+/*
+ * Copyright 2020 Luis Mejia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.util.Locale
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.4.20"
@@ -40,23 +55,23 @@ gradlePlugin {
     }
 }
 
-//// Add a source set for the functional test suite
-//val functionalTestSourceSet = sourceSets.create("functionalTest") {
-//}
-//
-//gradlePlugin.testSourceSets(functionalTestSourceSet)
-//configurations["functionalTestImplementation"].extendsFrom(configurations["testImplementation"])
-//
-//// Add a task to run the functional tests
-//val functionalTest by tasks.registering(Test::class) {
-//    testClassesDirs = functionalTestSourceSet.output.classesDirs
-//    classpath = functionalTestSourceSet.runtimeClasspath
-//}
-//
-//tasks.check {
-//    // Run the functional tests as part of `check`
-//    dependsOn(functionalTest)
-//}
+// Add a source set for functional test suite.
+val functionalTestSourceSet = sourceSets.create("functionalTest") {
+}
+
+gradlePlugin.testSourceSets(functionalTestSourceSet)
+configurations["functionalTestImplementation"].extendsFrom(configurations["testImplementation"])
+
+// Add a task to run functional tests.
+val functionalTest by tasks.registering(Test::class) {
+    testClassesDirs = functionalTestSourceSet.output.classesDirs
+    classpath = functionalTestSourceSet.runtimeClasspath
+}
+
+tasks.named("check") {
+    // Run functional tests as part of "check".
+    dependsOn(functionalTest)
+}
 
 //publishing {
 //    publications {
